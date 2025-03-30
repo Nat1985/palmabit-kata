ad86c6084f57c2ba01acddc9bfed96f207323bd6
Andrea Natale <andreanatale85@proton.me>
2025-03-30 18:01:45 +0200

    Updates

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,5 +1,5 @@
 import { Services } from "../pages/ServicesPage"
-import carDetailingIcon from '../assets/images/car-detailing.png'
+import carDetailingIcon from '../assets/images/car.png'
 import discBrakeIcon from '../assets/images/disc-brake.png'
 import oilIcon from '../assets/images/oil.png'
 import tireIcon from '../assets/images/tire.png'
@@ -8,14 +8,15 @@ import { useState } from "react"
 import AppointmentList from "./AppointmentList"
 
 const iconArray = [
-  carDetailingIcon,
-  discBrakeIcon,
   oilIcon,
-  tireIcon
+  discBrakeIcon,
+  tireIcon,
+  carDetailingIcon
 ]
 
 function ServiceCard({ id, serviceName, serviceDuration }: Services) {
   const [isSelected, setIsSelected] = useState<boolean>(false);
+  console.log(iconArray)
   return (
     <div className="w-full md:w-[600px] border rounded-lg p-4 border-[#5F7FCF]"
       onClick={() => setIsSelected(prev => !prev)}
@@ -23,7 +24,7 @@ function ServiceCard({ id, serviceName, serviceDuration }: Services) {
       {/* Titolo */}
       <div className="w-full flex gap-2 justify-between cursor-pointer">
         <div className="flex gap-6 items-center">
-          <img src={iconArray[id]} alt={`${serviceName} Icon`} className="w-12" />
+          <img src={iconArray[id - 1]} alt={`${serviceName} Icon`} className="w-12" />
           <h3 className="!text-black hidden md:block">{serviceName}</h3>
           <h4 className="!text-black md:hidden">{serviceName}</h4>
         </div>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 import logo from '../assets/images/logo.png'
-import { PrimaryButton } from '../components/buttons'
 
 function LandingPage() {
 
